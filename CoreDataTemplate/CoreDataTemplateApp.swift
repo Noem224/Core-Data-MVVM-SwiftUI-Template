e0a5af946c687b1aaac15d28e8c3d09846d41935
Daniel Wippermann <danielw83@live.de>
2023-02-20 13:35:29 +0100

    Initial Commit

---
+++ CoreDataTemplate/CoreDataTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataTemplateApp.swift
+//  CoreDataTemplate
+//
+//  Created by Daniel Wippermann on 20.02.23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
